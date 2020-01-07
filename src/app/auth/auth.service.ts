1920f286c7a0b603416eddd618fcade76013d1f3
domingoss <sebastiaomachava417@gmail.com>
2020-01-07 23:51:34 +0200

    Register

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import {Usuario} from './usuario.model';
 
 export interface AuthResponseData{
   estado: string;
@@ -36,6 +37,23 @@ export class AuthService {
     .post<AuthResponseData>("http://localhost/php/controller/login.php",{nome: nome, senha: senha});
 
 
+  }
+
+  register(nome: string, email: string, senha: string, confirma: string, endereco: string, contacto: string){
+
+      var postData = {
+
+          nome: nome,
+          email: email,
+          senha: senha,
+          confirma: confirma,
+          endereco: endereco,
+          contacto: contacto
+
+      }
+      
+      this.http
+      .post("http://php/controller/adicionar/add_utilizador.php",postData);
   }
 
   // register(nome:string,email:string, senha: string, confirmar:string, endereco:string, contacto: string ){
--- src/app/auth/registar/registar.page.ts
+++ src/app/auth/registar/registar.page.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import {NgForm} from '@angular/form'
 @Component({
   selector: 'app-registar',
   templateUrl: './registar.page.html',
@@ -9,6 +9,15 @@ export class RegistarPage implements OnInit {
 
   constructor() { }
 
+  onRegister(form: NgForm){
+    
+    const nome = formulario.value.nome;
+    const email = formulario.value.email;
+    const senha = formulario.value.senha;
+    const confirma = formulario.value.confirmar;
+    const endereco = formualrio.value.endereco;
+    const contacto = formulario.value.contacto;
+  }
   ngOnInit() {
   }
 
